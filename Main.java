b5853a35eda0462d6bc0fd8fe69fc2ed7b253dcf
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 00:55:38 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+//Main.java
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
+        int choice;
+
