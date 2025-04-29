8d8e8431cd459f71ee7d43f55c1b456aa94b2aee
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 01:08:15 +0530

    create search by position

--- StudentOperations.java
+++ StudentOperations.java
@@ -50,6 +50,14 @@ public class StudentOperations {
         throw new StudentNotFoundException("Student with name \"" + name + "\" not found.");
     }
 
+    // Search by Position (Index)
+    public Student searchByPosition(int index) throws InvalidInputException {
+        if (index < 0 || index >= students.size()) {
+            throw new InvalidInputException("Invalid index. Must be between 0 and " + (students.size() - 1));
+        }
+        return students.get(index);
+    }
+
     // Update student
     public boolean updateStudent(int prn, String name, String branch, String batch, float cgpa) throws StudentNotFoundException {
         Student s = searchByPRN(prn);
