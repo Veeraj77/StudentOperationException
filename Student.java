5e0ba1b0f84c472c588537a4262c4facde1cce83
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 01:10:21 +0530

    create student.java

---
+++ Student.java
@@ -0,0 +1,17 @@
+//Student.java
+
+public class Student {
+    private String name;
+    private int prn;
+    private String branch;
+    private String batch;
+    private float cgpa;
+
+    public Student(String name, int prn, String branch, String batch, float cgpa) {
+        this.name = name;
+        this.prn = prn;
+        this.branch = branch;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
+
