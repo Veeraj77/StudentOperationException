8581418996fed1e3c5e9f18b07202919588492c1
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 01:05:14 +0530

    create student operation

---
+++ StudentOperations.java
@@ -0,0 +1,11 @@
+//StudentOperations.java
+
+import java.util.*;
+
+public class StudentOperations {
+    private ArrayList<Student> students;
+
+    public StudentOperations() {
+        this.students = new ArrayList<>();
+    }
+
