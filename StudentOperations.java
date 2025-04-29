2903304660de2607ffed50b96cf5808c5ae7a9c8
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 01:05:49 +0530

    create add student

--- StudentOperations.java
+++ StudentOperations.java
@@ -9,3 +9,13 @@ public class StudentOperations {
         this.students = new ArrayList<>();
     }
 
+    // Add student
+    public void addStudent(Student student) throws StudentAlreadyExistsException {
+        for (Student s : students) {
+            if (s.getPRN() == student.getPRN()) {
+                throw new StudentAlreadyExistsException("Student with PRN " + student.getPRN() + " already exists.");
+            }
+        }
+        students.add(student);
+    }
+
